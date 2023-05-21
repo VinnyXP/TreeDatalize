8fdc35e349fcd9d2b814049048e9384c50e815d5
VinnyXP <viph0009@gmail.com>
2023-05-20 23:39:51 -0700

    update: initial react project

---
+++ tree-datalizer/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path = '/' element={} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
